6ac7de1239a669b31b51fac53872e8b5e25ce042
weichen11235 <44263103+weichen11235@users.noreply.github.com>
2020-04-30 15:49:30 -0400

    joke api

--- app.js
+++ app.js
@@ -17,3 +17,22 @@ $("#relax").click(function(){
     $("#relax").text("wanna relax?");
   }
 });
+
+//JOKE API
+$("#get-joke").click(function(){
+  $.get("http://api.icndb.com/jokes/random", function(data, status){
+    $("#sj-content").text(data.value.joke);
+  });
+});
+
+$("#get-jokes").click(function(){
+  let type = $("#category").val();
+  let number = $("#joke-number").val();
+  $.get(`http://api.icndb.com/jokes/random/${number}?limitTo=[${type}]`, function(data, status){
+    let content = "";
+    data.value.forEach(function(item){
+      content += `<p>${item.joke}</p>`;
+    })
+    $("#mj-content").html(content);
+  });
+});
